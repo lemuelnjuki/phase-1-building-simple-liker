ee37a056a2f5345f9641911289ec1e7c3899216d
lemuelnjuki <lemuel.njuki@student.moringaschool.com>
2022-08-25 22:28:41 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -3,8 +3,34 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal = document.getElementById("modal");
+modal.className = "hidden";
 
 
+function errorHearts(e) {
+  mimicServerCall()
+  .then(heartColor(e))
+  .catch(error => {
+    errorModel.innerHTML = error
+    errorModel.className = " "
+    setTimeout(() => errorModel.className = "hidden", 3000)
+  })
+}
+
+function heartColor(e) {
+  const heart = e.target
+  if (heart.innerText === EMPTY_HEART) {
+    heart.innerText = FULL_HEART
+    heart.className="activated-heart"
+  } else {
+    heart.innerText = EMPTY_HEART
+    heart.className="activated-heart"
+  }
+}
+const hearts = document.querySelectorAll(".like-glyph");
+for (let heart of hearts) {
+  heart.addEventListener("click", errorHearts);
+}
 
 
 //------------------------------------------------------------------------------
